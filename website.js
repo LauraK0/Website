0da77201bd39052009b92f9d223e1741fb65c333
LauraK0 <laurakeating@live.co.uk>
2022-11-13 16:49:46 +0000

    styling changed

--- website.js
+++ website.js
@@ -1,71 +1,24 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+const cont = document.getElementById("container");
+console.log(cont);
+let elsArr = [].slice.call(document.querySelectorAll('.el'));
+let closeBtnsArr = [].slice.call(document.querySelectorAll('.el__close-btn'));
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+setTimeout(function() {
+  cont.classList.remove('s--inactive');
+}, 200);
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("card");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-}
-
-
-const state = {};
-const carouselList = document.querySelector('.carousel-list');
-const carouselItems = document.querySelectorAll('.carousel-item');
-const elems = Array.from(carouselItems);
-
-carouselList.addEventListener('click', function (event) {
-  var newActive = event.target;
-  var isItem = newActive.closest('.carousel-item');
-
-  if (!isItem || newActive.classList.contains('carousel-item_active')) {
-    return;
-  };
-  
-  update(newActive);
+elsArr.forEach(function(el) {
+  el.addEventListener('click', function() {
+    if (this.classList.contains('s--active')) return;
+    cont.classList.add('s--el-active');
+    this.classList.add('s--active');
+  });
 });
 
-const update = function(newActive) {
-  const newActivePos = newActive.dataset.pos;
-
-  const current = elems.find((elem) => elem.dataset.pos == 0);
-  const prev = elems.find((elem) => elem.dataset.pos == -1);
-  const next = elems.find((elem) => elem.dataset.pos == 1);
-  const first = elems.find((elem) => elem.dataset.pos == -2);
-  const last = elems.find((elem) => elem.dataset.pos == 2);
-  
-  current.classList.remove('carousel-item_active');
-  
-  [current, prev, next, first, last].forEach(item => {
-    var itemPos = item.dataset.pos;
-
-    item.dataset.pos = getPos(itemPos, newActivePos)
+closeBtnsArr.forEach(function(btn) {
+  btn.addEventListener('click', function(e) {
+    e.stopPropagation();
+    cont.classList.remove('s--el-active');
+    document.querySelector('.el.s--active').classList.remove('s--active');
   });
-};
-
-const getPos = function (current, active) {
-  const diff = current - active;
-
-  if (Math.abs(current - active) > 2) {
-    return -current
-  }
-
-  return diff;
-}
+});
